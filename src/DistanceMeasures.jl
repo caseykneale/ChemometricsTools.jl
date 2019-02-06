7822c9c81f082db3e24f57033b04d34db34ffdfc
CaseyKneale <caseykneale@localhost.localdomain>
2019-02-06 18:25:01 -0500

    Added shortcut manhattan distance..

--- src/DistanceMeasures.jl
+++ src/DistanceMeasures.jl
@@ -2,10 +2,21 @@ SquareEuclideanDistance(X, Y) = ( sum(X .^ 2, dims = 2) .+ sum(Y .^ 2, dims = 2)
 
 EuclideanDistance(X, Y) = sqrt.(SquareEuclideanDistance(X, Y))
 
+#Only computes half distance and mirrors it...
+function ManhattanDistance(X)
+    Result = zeros(size(X)[1], size(X)[1])
+    for rowx in 2 : (size(X)[1]), rowy in 1 : (rowx-1)
+        Result[rowx, rowy] = sum( abs.( X[rowx,:] - X[rowy,:]  ) )
+        Result[rowy, rowx] = Result[rowx, rowy]
+    end
+    return Result
+end
+
 function ManhattanDistance(X, Y)
     Result = zeros(size(X)[1], size(Y)[1])
-    for rowx in 1 : size(X)[1], rowy in 1 : size(Y)[1]
+    for rowx in 1 : (size(X)[1]), rowy in 1 : size(Y)[1]
         Result[rowx, rowy] = sum( abs.( X[rowx,:] - Y[rowy,:]  ) )
+        Result[rowy, rowx] = Result[rowx, rowy]
     end
     return Result
 end
