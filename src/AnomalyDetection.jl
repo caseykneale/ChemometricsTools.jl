5ce501d81fae53020a0b297dae38c3ce185a9e1f
CaseyKneale <caseykneale@localhost.localdomain>
2019-02-10 16:08:33 -0500

    Finished Q statistic and tested it

--- src/AnomalyDetection.jl
+++ src/AnomalyDetection.jl
@@ -1,10 +1,12 @@
 using Distributions
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-include("/home/caseykneale/Desktop/Spectroscopy/chemotools/ChemometricsTools/src/Analysis.jl")
+# include("/home/caseykneale/Desktop/Spectroscopy/chemotools/ChemometricsTools/src/Analysis.jl")
 
-#Untested. Not super optimal, but should work fine, for small data...
 #Ideally I'd only access each column once in the K loop... Tricky...
+#Untested. Not super optimal, but should work fine, for small data...
+#Could store inter distance matrix to a struct...
+#Untested...
 function OneClassKNN( Normal, New; J = 1, K = 1, threshold = 1.0 )
     Obs = size( Normal )[ 1 ]
     ObsNew = size( New )[ 1 ]
@@ -45,24 +47,6 @@ function OneClassKNN( Normal, New; J = 1, K = 1, threshold = 1.0 )
     return (IntraDataMeanDist ./ InterDataMeanDist) .< Threshold
 end
 
-function ( model::OneClassKNN )( Z; K = 1 )
-    DistMat = zeros( size( model.X )[ 1 ], size( Z )[ 1 ] )
-    Predictions = zeros( size( Z )[ 1 ] )
-    #Apply Distance Fn
-    if model.DistanceType == "euclidean"
-        DistMat = SquareEuclideanDistance(model.X, Z)
-    elseif model.DistanceType == "manhattan"
-        DistMat = ManhattanDistance(model.X, Z)
-    end
-    #Find nearest neighbors and majority vote
-    for obs in 1 : size( Z )[ 1 ]
-        Preds = sortperm( DistMat[:, obs] )[ 1 : K ]
-        Predictions[ obs ] = argmax( StatsBase.countmap( model.Y[ Preds ] ) )
-    end
-
-    return Predictions
-end
-
 function Hotelling(X, pca::PCA; Significance = 0.05, Variance = 1.0)
     (Obs,Vars) = size(X)
     CumVar = cumsum( ExplainedVariance( pca ) )
@@ -90,30 +74,19 @@ function Leverage(pca::PCA)
 end
 
 
-#Incomplete
 #https://wis.kuleuven.be/stat/robust/papers/2013/deketelaere-review.pdf
-function Q(X, pca::PCA; Significance = 0.05, Variance = 0.99)
+function Q(X, pca::PCA; Significance = 0.05, Variance = 0.95)
     (Obs,Vars) = size(X)
     CumVar = cumsum( ExplainedVariance( pca ) )
     PCs = sum(CumVar .<= Variance)
-    Q = X * (LinearAlgebra.Diagonal(ones(Vars)) - pca.Loadings[1:PCs,:]' * pca.Loadings[1:PCs,:]) * X'
-     #Correct up to this point... The rest well... Gonna need to figure it out
-     # L = cumsum(pca.Values[ 1 : 3 ])
-     # H0 = 1.0 - ( ( 2.0 * L[ 1 ] * L[ 3 ] ) / ( 3.0 * L[ 2 ]^2 ) )
-     # Gauss = quantile( Normal(), 1.0 - Significance )
-     # Upper = L[1] * ( ( ( H0 * Gauss * sqrt( 2.0 * L[2] ) ) / L[1] ) + 1.0 + ( ( L[2] * H0 * ( H0 - 1.0 )/ L[1]^2) )^(1.0/H0))
-    return Q#, Upper
+    Q = diag(X * (LinearAlgebra.Diagonal(ones(Vars)) - pca.Loadings[1:PCs,:]' * pca.Loadings[1:PCs,:]) * X')
+    #Correct up to this point... The rest well... Gonna need to figure it out
+    L = [ sum(pca.Values[(PCs + 1) : end] .^ order) for order in [ 1, 2, 3 ] ]
+    H0 = 1.0 - ( ( 2.0 * L[ 1 ] * L[ 3 ] ) / ( 3.0 * L[ 2 ]^2 ) )
+    Gauss = quantile( Normal(), Significance )
+    FirstTerm = ( Gauss * sqrt( 2.0 * L[2] * H0^2 ) ) / L[1]
+    SecondTerm =  (L[2] * H0 * ( 1.0 - H0 ) ) / L[1]^2
+
+    Upper = L[1] * ( FirstTerm + 1.0 + SecondTerm ) ^ (2)
+    return Q, Upper
 end
-#Q(test, pca_obj; Variance = 1.0)
-
-
-# using Plots
-# test = vcat( randn(100,4), 7 .* randn(3,4) ) ;
-#
-# pca_obj = PCA(test; Factors = 2)
-#
-# (tsq, t) = Hotelling(test, pca_obj; Variance = 1.0)
-#
-# scatter( pca_obj.Scores[:,1], pca_obj.Scores[:,2])
-# a = plot(tsq);
-# Plots.abline!(a, 0, t, label = "Control Limit")
