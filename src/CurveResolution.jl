add8e677037d16b4120c73a3fc88975777b55fc2
CaseyKneale <caseykneale@localhost.localdomain>
2019-02-14 07:33:53 -0500

    FNNLS first pass complete... Trying to do MCR-ALS...

--- src/CurveResolution.jl
+++ src/CurveResolution.jl
@@ -9,6 +9,7 @@ function NMF(X; Factors = 1, tolerance = 1e-7, maxiters = 200)
     Last = zeros(Obs,Vars)
     tolerancesq = tolerance ^ 2
     iters = 0
+    #Monitor change in F norm...
     while (sum( ( Last .- W * H ) .^ 2)  > tolerancesq) && (iters < maxiters)
         Last = W * H
         H .*= ( W' * X ) ./ ( W' * W * H )
@@ -41,7 +42,6 @@ function FNNLS(A, b;
                 tolerance = 1e-6 * prod( size( A ) ),
                 maxiters = 150)
     eps = 1e-6
-
     ATA = A' * A
     ATb = A' * b
     P = zeros( size( ATA )[ 2 ] ) .|> Int
@@ -50,6 +50,7 @@ function FNNLS(A, b;
     X = zeros( 1, size( ATA )[ 2 ] )
     Inds = collect( 1 : length( ATb ) ) .|> Int
     Rinds = collect( 1 : length( ATb ) ) .|> Int
+
     W = ATb .- (ATA * X')
     inneriters = 0
     iter = 0
@@ -92,29 +93,90 @@ function FNNLS(A, b;
 end
 
 #Torture test...
-# for i in 1:10000
-#     a = rand(4,4);
-#     b = rand(4);
-#     x = FNNLS( a,  b)
-#     if any(x .< 0.0)
-#         println("ahhh")
-#     end
-# end
+for i in 1:10000
+    a = rand(4,4);
+    b = rand(4);
+    x = FNNLS( a,  b)
+    if any(x .< 0.0)
+        println("ahhh")
+    end
+end
+using LinearAlgebra
+#Trying to do this from memory.. X = S C
+#So that. S = (XtX)-1 C && C = S (XtX)-1
+function MCRALS(X, C, S = nothing;
+                Factors = 1, maxiters = 50,
+                nonnegative = true)
+    @assert all( isa.( [ C , S ], Nothing ) ) == false
+    err = Inf
+    D = X
+    isC = isa(C, Nothing)
+    isS = isa(S, Nothing)
+    C = isa(C, Nothing) ? zeros(size(X)[1], Factors) : C
+    S = isa(S, Nothing) ? zeros(Factors, size(X)[2]) : S
+    #Let's burn in our parameter estimates...
+    for iter in 1 : maxiters
+        if !isS
+            if nonnegative
+                println(size(D))
+                println(size(S))
+                for col in 1:Factors
+                    C[:,col] = FNNLS(D, S[col,:])
+                end
+            else
+                C = (S * LinearAlgebra.pinv(D)')'
+            end
+            isS = false
+            D = (C * S)'
+        end
+        if !isC
+            if nonnegative
+                for col in 1:size(C)[2]
+                    S[col,:] = FNNLS(D, C[:,col])
+                end
+            else
+                S = C' * LinearAlgebra.pinv(D)
+            end
+            isC = false
+            D = (C * S)'
+        end
+        err = sum( ( X .- D ) .^ 2 )
+    end
+    return ( C, S, err )
+end
 
 
+vec(W[:,1])
+size(Fraud)
+
+
+( C, S, err ) = MCRALS(Fraud, nothing , W'; Factors = 2)
+size(S)
+
+
+using CSV
+using DataFrames
+Raw = CSV.read("/home/caseykneale/Desktop/Spectroscopy/Data/MIR_Fruit_purees.csv");
+Lbls = convert.(Bool, occursin.( "NON", String.(names( Raw )) )[2:end]);
+Dump = collect(convert(Array, Raw)[:,2:end]');
+Fraud = Dump[Lbls,:];
+
+using Plots
+using LinearAlgebra
+size(svd(Fraud).U)
+
+
+
+(W, H) = NMF(Fraud; Factors = 2)
+size(W)
+
+size(C)
+size(S)
+#plot(svd(Fraud).V[:,1:2])
+plot(C)
+
+
+( W, H ) = NMF(Fraud; Factors = 3, maxiters = 200, tolerance = 1e-6)
+plot((H)')
 #
-# using CSV
-# using DataFrames
-# Raw = CSV.read("/home/caseykneale/Desktop/Spectroscopy/Data/MIR_Fruit_purees.csv");
-# Lbls = convert.(Bool, occursin.( "NON", String.(names( Raw )) )[2:end]);
-# Dump = collect(convert(Array, Raw)[:,2:end]');
-# Fraud = Dump[Lbls,:];
-#
-#
-# (W,H) = NMF(Fraud; Factors = 3, maxiters = 200, tolerance = 1e-6)
-#
-# using Plots
-#
-# plot((H)')
-#
-# plot(Fraud')
+plot(Fraud')
