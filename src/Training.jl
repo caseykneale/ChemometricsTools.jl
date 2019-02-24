b5fe9615332efdf81d40366e4d9ac19c167086d7
CaseyKneale <caseykneale@localhost.localdomain>
2019-02-24 10:45:06 -0500

    Tackled K-folds edge cases...

--- src/Training.jl
+++ src/Training.jl
@@ -13,29 +13,44 @@ struct KFoldsValidation
     K::Int
     FoldSize::Int
     observations::Int
+    remainder::Int
     X
     Y
 end
+forceMatrix(a) = (length(size(a)) == 1) ? reshape( a, length(a), 1 ) : a
 
-KFoldsValidation(K, x, y) = KFoldsValidation( K, floor(size( x )[ 1 ] / K), size( x )[ 1 ], x, y)
-LeaveOneOut(x,y) = KFoldsValidation( size( x )[ 1 ], 1, size( x )[ 1 ], x, y )
+function KFoldsValidation(K::Int, x, y)
+    observations = size( x )[ 1 ]
+    if floor(size( x )[ 1 ] / K) == 1
+        K = observations
+        println("Warning: Hold out size is 1, defaulting to Leave One Out Validation")
+    end
+    Remainder = observations % K
+    KFoldsValidation( K, floor(size( x )[ 1 ] / K), observations, Remainder, x, forceMatrix(y))
+end
+LeaveOneOut(x,y) = KFoldsValidation( size( x )[ 1 ], 1, size( x )[ 1 ], 0, x, forceMatrix(y) )
 
-function Base.iterate( iter::KFoldsValidation, state = 0 )
-    i = state;
+
+function Base.iterate( iter::KFoldsValidation, state = (0,0) )
+    ( i, o ) = state;
     predictInds = []; trainInds = []; holdoutInds = []
+    r = ( ( iter.K - i) <= iter.remainder) ? 1 : 0
 
     if i == 0
         holdoutInds = collect( 1 : iter.FoldSize )
         trainInds = collect( ( iter.FoldSize + 1 ) : iter.observations )
     elseif i < iter.K
-        holdoutInds = collect( ( ( i * iter.FoldSize ) + 1 ) : ( ( i + 1 ) * iter.FoldSize ) )
-        trainInds = [collect( 1 : ( i * iter.FoldSize ) );
-                    collect( ( ( ( i + 1 ) * iter.FoldSize ) + 1 ) : iter.observations ) ]
+        HOBegin = ( i * iter.FoldSize ) + 1 + (o)
+        HOEnd = ( i + 1 ) * iter.FoldSize + o + r
+        TBegin = collect( 1 : (HOBegin - 1) )
+        TEnd = collect( ( HOEnd + 1 ) : iter.observations )
+        holdoutInds = collect( HOBegin : HOEnd )
+        trainInds = [ TBegin; TEnd]
     else
         return nothing
     end
-
-    return (    ((iter.X[trainInds,:], iter.Y[trainInds]),
-                ((iter.X[holdoutInds,:], iter.Y[holdoutInds]))),
-                (i + 1) )
+    return (    (   ( iter.X[trainInds,:]     , iter.Y[trainInds,:] ),
+                    ( ( iter.X[holdoutInds,:]   , iter.Y[holdoutInds,:] ) )
+                ),
+                ( i + 1 , o + r) )
 end
